a262db3b5e94e228fd994a26c5b6651714aa52ec
hsmtkk <hsmtkk@gmail.com>
2022-01-22 16:43:10 +0900

    message forwarder

--- forward/forward.go
+++ forward/forward.go
@@ -1,1 +1,76 @@
 package main
+
+import (
+	"fmt"
+	"log"
+	"strconv"
+
+	"github.com/go-redis/redis"
+	"github.com/hsmtkk/cuddly-waffle/env"
+	"github.com/hsmtkk/cuddly-waffle/msg"
+	"github.com/nats-io/nats.go"
+	"go.uber.org/zap"
+)
+
+func main() {
+	natsHost := env.RequiredString("NATS_HOST")
+	natsPort := env.RequiredInt("NATS_PORT")
+	natsChannel := env.RequiredString("NATS_CHANNEL")
+	redisHost := env.RequiredString("REDIS_HOST")
+	redisPort := env.RequiredInt("REDIS_PORT")
+
+	logger, err := zap.NewDevelopment()
+	if err != nil {
+		log.Fatalf("failed to init logger; %s", err)
+	}
+	defer logger.Sync()
+	sugar := logger.Sugar()
+
+	redisAddr := fmt.Sprintf("%s:%d", redisHost, redisPort)
+	redisClient := redis.NewClient(&redis.Options{
+		Addr:     redisAddr,
+		Password: "", // no password set
+		DB:       0,  // use default DB
+	})
+	defer redisClient.Close()
+
+	natsAddr := fmt.Sprintf("nats://%s:%d", natsHost, natsPort)
+	natsConn, err := nats.Connect(natsAddr)
+	if err != nil {
+		sugar.Fatalw("failed to connect NATS", "address", natsAddr, "error", err)
+	}
+	defer natsConn.Close()
+
+	handler := newMessageHandler(sugar, redisClient)
+
+	sub, err := natsConn.Subscribe(natsChannel, handler.Handle)
+	if err != nil {
+		sugar.Fatalw("failed to subscribe channel", "channel", natsChannel, "error", err)
+	}
+	defer sub.Unsubscribe()
+
+	select {}
+}
+
+type messageHandler struct {
+	sugar       *zap.SugaredLogger
+	redisClient *redis.Client
+}
+
+func newMessageHandler(sugar *zap.SugaredLogger, redisClient *redis.Client) *messageHandler {
+	return &messageHandler{sugar, redisClient}
+}
+
+func (hdl *messageHandler) Handle(natsMsg *nats.Msg) {
+	natsMsgStr := string(natsMsg.Data)
+	m, err := msg.FromJSON(natsMsg.Data)
+	if err != nil {
+		hdl.sugar.Errorw("failed to decode message", "message", natsMsgStr, "error", err)
+		return
+	}
+	if err := hdl.redisClient.Set(strconv.FormatInt(m.ID, 10), natsMsgStr, 0).Err(); err != nil {
+		hdl.sugar.Errorw("failed to set redis", "error", err)
+		return
+	}
+	return
+}
--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module github.com/hsmtkk/cuddly-waffle
 go 1.17
 
 require (
+	github.com/go-redis/redis v6.15.9+incompatible
 	github.com/nats-io/nats.go v1.13.1-0.20211122170419-d7c1d78a50fc
 	github.com/spf13/cobra v1.3.0
 	go.uber.org/zap v1.17.0
@@ -16,6 +17,8 @@ require (
 	github.com/nats-io/nats-server/v2 v2.7.0 // indirect
 	github.com/nats-io/nkeys v0.3.0 // indirect
 	github.com/nats-io/nuid v1.0.1 // indirect
+	github.com/onsi/ginkgo v1.16.4 // indirect
+	github.com/onsi/gomega v1.16.0 // indirect
 	github.com/spf13/pflag v1.0.5 // indirect
 	go.uber.org/atomic v1.7.0 // indirect
 	go.uber.org/multierr v1.6.0 // indirect
